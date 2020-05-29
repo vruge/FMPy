75e295ce343b0b801cc53628cb3dc237466d8ae4
Torsten Sommer <torsten.sommer@3ds.com>
2020-05-29 10:47:38 +0200

    Build with build_cvode.py on macOS in CI

--- build_cvode.py
+++ build_cvode.py
@@ -1,20 +1,41 @@
+from fmpy import platform_tuple, sharedLibraryExtension
+from fmpy.util import download_file
 import tarfile
-import requests
 import os
 import shutil
 from subprocess import check_call
 
+
+if os.name == 'nt':
+    generator = 'Visual Studio 15 2017 Win64'
+    sl_prefix = ''
+    sl_suffix = sharedLibraryExtension
+else:
+    generator = 'Unix Makefiles'
+    sl_prefix = 'lib'
+    sl_suffix = sharedLibraryExtension
+
+sundials_binary_dir = os.path.join('fmpy', 'sundials', platform_tuple)
+
+# clean up
+for build_dir in ['cswrapper/build', 'cvode-5.3.0', sundials_binary_dir]:
+    if os.path.isdir(build_dir):
+        shutil.rmtree(build_dir)
+
 url = 'https://computing.llnl.gov/projects/sundials/download/cvode-5.3.0.tar.gz'
+checksum = 'd7ff8e77bb2a59cf8143de30f05a2651c2b4d29b586f8003f9187bf9e5a7da6e'
+
 filename = os.path.basename(url)
 
-response = requests.get(url)
+download_file(url, checksum)
 
-with open(filename, 'wb') as f:
-    f.write(response.content)
+# response = requests.get(url)
+#
+# with open(filename, 'wb') as f:
+#     f.write(response.content)
 
 with tarfile.open(filename, "r:gz") as tar:
     tar.extractall()
-    tar.close()
 
 os.mkdir('cvode-5.3.0/static')
 
@@ -25,7 +46,7 @@ check_call([
     '-DBUILD_SHARED_LIBS=OFF',
     '-DCMAKE_INSTALL_PREFIX=cvode-5.3.0/static/install',
     '-DCMAKE_USER_MAKE_RULES_OVERRIDE=../OverrideMSVCFlags.cmake',
-    '-G', 'Visual Studio 15 2017 Win64',
+    '-G', generator,
     '-S', 'cvode-5.3.0',
     '-B', 'cvode-5.3.0/static'
 ])
@@ -39,19 +60,21 @@ check_call([
     '-DBUILD_STATIC_LIBS=OFF',
     '-DCMAKE_INSTALL_PREFIX=cvode-5.3.0/dynamic/install',
     '-DCMAKE_USER_MAKE_RULES_OVERRIDE=../OverrideMSVCFlags.cmake',
-    '-G', 'Visual Studio 15 2017 Win64',
+    '-G', generator,
     '-S', 'cvode-5.3.0',
     '-B', 'cvode-5.3.0/dynamic'
 ])
 
 check_call(['cmake', '--build', 'cvode-5.3.0/dynamic', '--target', 'install', '--config', 'Release'])
 
-os.mkdir('fmpy/sundials/x86_64-windows')
+os.mkdir(sundials_binary_dir)
 
-shutil.copy('cvode-5.3.0/dynamic/install/lib/sundials_cvode.dll',          'fmpy/sundials/x86_64-windows')
-shutil.copy('cvode-5.3.0/dynamic/install/lib/sundials_nvecserial.dll',     'fmpy/sundials/x86_64-windows')
-shutil.copy('cvode-5.3.0/dynamic/install/lib/sundials_sunlinsoldense.dll', 'fmpy/sundials/x86_64-windows')
-shutil.copy('cvode-5.3.0/dynamic/install/lib/sundials_sunmatrixdense.dll', 'fmpy/sundials/x86_64-windows')
+os.path.join('cvode-5.3.0', 'dynamic', 'install', 'sundials_cvode' + sharedLibraryExtension)
+
+for name in ['sundials_cvode', 'sundials_nvecserial', 'sundials_sunlinsoldense', 'sundials_sunmatrixdense']:
+    src = os.path.join('cvode-5.3.0', 'dynamic', 'install', 'lib', sl_prefix + name + sl_suffix)
+    dst = os.path.join(sundials_binary_dir, name + sl_suffix)
+    shutil.copy(src, dst)
 
 # build cswrapper
 os.mkdir('cswrapper/build')
@@ -59,7 +82,7 @@ os.mkdir('cswrapper/build')
 check_call([
     'cmake',
     '-DCVODE_INSTALL_DIR=../cvode-5.3.0/static/install',
-    '-G', 'Visual Studio 15 2017 Win64',
+    '-G', generator,
     '-S', 'cswrapper',
     '-B', 'cswrapper/build'
 ])
