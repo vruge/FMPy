0ae90837350b3c4ee10979e45102473152f26522
Torsten Sommer <torsten.sommer@3ds.com>
2020-10-24 00:55:15 +0200

    Fix style

--- dash/basic_callback.py
+++ dash/basic_callback.py
@@ -7,24 +7,51 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from fmpy import *
 
+filename = r'E:\Development\Reference-FMUs\build\dist\BouncingBall.fmu'
+
+model_description = read_model_description(filename)
+
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+names = []
+rows = []
+states = []
+
+for variable in model_description.modelVariables:
+    if variable.causality == 'parameter':
+
+        unit = variable.unit
+
+        if unit is None and variable.declaredType is not None:
+            unit = variable.declaredType.unit
+
+        names.append(variable.name)
+        row = dbc.Row([
+            dbc.Col([dbc.Label(variable.name, className='col-form-label')], width=2),
+            dbc.Col([
+                # [dbc.Input(id=variable.name, value=variable.start, type='text')], width=4),
+                dbc.InputGroup(
+                    [
+                        dbc.Input(id=variable.name, value=variable.start),
+                        dbc.InputGroupAddon(unit if unit else '', addon_type="append"),
+                    ], size="sm"
+                    # className="mb-3"
+                )
+            ]),
+            dbc.Col([html.Span(variable.description, className="text-muted align-middle",
+                               style={'margin-top': 'auto', 'margin-bottom': 'auto'})], width=6),
+        ])
+        rows.append(row)
+        states.append(State(variable.name, 'value'))
 
 app.layout = html.Div([
     dbc.Container([
-        html.H1("ModelName", className="display-4"),
-        html.P("Description", className="lead")
+        html.H1(model_description.modelName, className="display-4"),
+        html.P(model_description.description, className="lead")
     ], className="mx-auto text-center mt-5"),
-    dbc.Container([
-        dbc.Row([
-            dbc.Col([dbc.Label("name", className='col-form-label')], width=2),
-            dbc.Col([dbc.Input(id='my-input', value='1.0', type='text')], width=4),
-            dbc.Col([html.Span("description", className="text-muted align-middle", style={'margin-top': 'auto', 'margin-bottom': 'auto'})], width=6),
-        ],), # no_gutters=True,
-        dbc.Button("Simulate", color="primary", className="btn-block mt-5", id='submit-button-state'),
-    ], className="pt-5"),
+    dbc.Container(rows + [dbc.Button("Simulate", color="primary", className="btn-block mt-5", id='submit-button-state')], className="pt-5"),
     # html.Div(["Input: ",
     #           ]),
     # html.Br(),
@@ -43,15 +70,17 @@ app.layout = html.Div([
     # Output(component_id='my-output', component_property='children'),
     Output('example-graph', 'figure'),
     [Input('submit-button-state', 'n_clicks')],
-    [State('my-input', 'value')]
+    #[State('my-input', 'value')]
+    states
 )
-def update_output_div(n_clicks, value):
+def update_output_div(n_clicks, *values):
+
+    start_values = dict(zip(names, values))
 
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
 
-
-    result = simulate_fmu(filename=r'E:\Development\Reference-FMUs\build\dist\BouncingBall.fmu',
-                          start_values={'h_init': value})
+    result = simulate_fmu(filename=filename,
+                          start_values=start_values)
 
     if n_clicks:
         fig.add_trace(go.Scatter(x=result['time'], y=result['height']), row=1, col=1)
