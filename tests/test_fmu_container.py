ce1985206e835021eea9196a535b0aa7e30978b5
Torsten Sommer <torsten.sommer@3ds.com>
2020-12-11 13:55:39 +0100

    Add ME wrapper test

--- tests/test_fmu_container.py
+++ tests/test_fmu_container.py
@@ -1,13 +1,16 @@
 import os
 import unittest
-from fmpy import simulate_fmu, plot_result
+from fmpy import simulate_fmu, plot_result, read_model_description
 from fmpy.fmucontainer import create_fmu_container
 import numpy as np
 
+from fmpy.util import download_test_file
 
-@unittest.skipIf('SSP_STANDARD_DEV' not in os.environ, "Environment variable SSP_STANDARD_DEV must point to the clone of https://github.com/modelica/ssp-standard-dev")
+
 class FMUContainerTest(unittest.TestCase):
 
+    @unittest.skipIf('SSP_STANDARD_DEV' not in os.environ,
+                     "Environment variable SSP_STANDARD_DEV must point to the clone of https://github.com/modelica/ssp-standard-dev")
     def test_create_fmu_container(self):
 
         examples = os.path.join(os.environ['SSP_STANDARD_DEV'], 'SystemStructureDescription', 'examples')
@@ -69,3 +72,45 @@ class FMUContainerTest(unittest.TestCase):
         result = simulate_fmu(filename, start_values={'k': 20}, input=w_ref, output=['w_ref', 'w'], stop_time=4)
 
         # plot_result(result)
+
+    def test_wrap_model_exchange(self):
+
+        filename = 'CoupledClutches.fmu'
+
+        download_test_file('2.0', 'me', 'MapleSim', '2016.2', 'CoupledClutches', filename)
+
+        model_description = read_model_description(filename)
+
+        variable_names = [v.name for v in model_description.modelVariables]
+
+        configuration = {
+
+            'description': model_description.description,
+
+            'variables': dict(('instance.' + n, {'name': n}) for n in variable_names),
+
+            'components':
+                [
+                    {
+                        'filename': filename,
+                        'interfaceType': 'ModelExchange',
+                        'name': 'instance',
+                        'variables': variable_names
+                    }
+                ],
+
+            'connections': []
+
+        }
+
+        if model_description.defaultExperiment:
+            configuration['defaultExperiment'] = {
+                'startTime': model_description.defaultExperiment.startTime,
+                'stopTime': model_description.defaultExperiment.stopTime,
+                'stepSize': model_description.defaultExperiment.stepSize,
+                'tolerance': model_description.defaultExperiment.tolerance,
+            }
+
+        create_fmu_container(configuration, 'CoupledClutchesCS.fmu')
+
+        simulate_fmu('CoupledClutchesCS.fmu')
